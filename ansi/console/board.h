60f3b78a84dda23b6f934614289be3760a947118
Humay Abbasli <humayabbasli@gmail.com>
2022-06-11 22:52:03 +0400

    documented board.h

--- ansi/console/board.h
+++ ansi/console/board.h
@@ -1,3 +1,14 @@
+/**
+ * @file board.h
+ * @author Firangiz Aslanova and Humay Abbasli
+ * @brief Header file for board functions
+ * @version 1.0
+ * @date 2022-06-11
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
 #ifndef _BOARD_H_
 #define _BOARD_H_
 
@@ -6,7 +17,13 @@
 #include <unistd.h>
 #include <time.h>
  
-
+/**
+ * @brief Display a board
+ * This function displays a board with live cells denoted by white color and dead cells by empty space
+ * @param board 
+ * @param width the width of the board
+ * @param height the height of the board
+ */
 void show(void *board, int width, int height);
 
 
